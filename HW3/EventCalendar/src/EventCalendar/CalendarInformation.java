ff2319b1a58913d41c6a293babef7537047b0a5a
jnuez16 <frozen.nuez@gmail.com>
2015-01-16 14:44:25 -0800

    Revert "Didn't work need to change some"
    This reverts commit 1021ecbbe169a38d6987273fc5bf0c6d6a7d788a.

--- HW3/EventCalendar/src/EventCalendar/CalendarInformation.java
+++ HW3/EventCalendar/src/EventCalendar/CalendarInformation.java
@@ -15,51 +15,44 @@ import java.io.IOException;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.swing.DefaultListModel;
 
 /**
  *
  * @author jnuez_000
  */
 public class CalendarInformation {
-    //private Vector<String> title = new Vector<String>();
-    //private Vector<String> loc = new Vector<String>();
-    //private Vector<String> date = new Vector<String>();
-    private Vector<Event> ev = new Vector<Event>();
-    private DefaultListModel<String> model = new DefaultListModel<String>();
-    private File f = new File("C:\\Users\\jnuez16\\Documents\\CS372-Java\\HW3\\EventCalendar\\Calendar.txt");
+    private Vector<String> title = new Vector<String>();
+    private Vector<String> loc = new Vector<String>();
+    private Vector<String> date = new Vector<String>();
+    private File f = new File("C:\\Users\\jnuez_000\\Documents\\GitHub\\CS372-Java\\HW3\\EventCalendar\\Calendar.txt");
     
     public CalendarInformation() throws FileNotFoundException, IOException
     {
-        String t = "";
-        String l = "";
-        String d = "";
         try{
             BufferedReader rdr = new BufferedReader(new FileReader(f));
             String line;
             while((line = rdr.readLine()) != null)
             {
-                Pattern p = Pattern.compile("title: (.*)<t>");
+                Pattern p = Pattern.compile("title: (.*);");
                 Matcher m = p.matcher(line);
                 while(m.find())
                 {
-                    t = m.group(1);
+                    title.add(m.group(1));
                 }
                 
-                p = Pattern.compile("location: (.*)<l>");
+                p = Pattern.compile("location: (.*);");
                 m = p.matcher(line);
                 while(m.find())
                 {
-                    l = m.group(1);
+                    loc.add(m.group(1));
                 }
                 
-                p = Pattern.compile("Date: (.*)<d>");
+                p = Pattern.compile("Date: (.*);");
                 m = p.matcher(line);
                 while(m.find())
                 {
-                    d = m.group(1);
+                    date.add(m.group(1));
                 }
-                ev.add(new Event(t,l,d));
             }
             rdr.close();
         }
@@ -79,9 +72,9 @@ public class CalendarInformation {
         try{
             BufferedReader rdr = new BufferedReader(new FileReader(f));
             BufferedWriter wrtr = new BufferedWriter(new FileWriter(f));
-            for(int i = 0; i < ev.size(); i++)
+            for(int i = 0; i < title.size(); i++)
             {
-                wrtr.write(String.format("title: %s<t> location: %s<l> Date: %s<d>\n", ev.get(i).getTitle(), ev.get(i).getLoc(), ev.get(i).getDate()));
+                wrtr.write(String.format("title: %s;\nlocation: %s;\nDate: %s;\n", title.get(i), loc.get(i), date.get(i)));
             }
             rdr.close();
             wrtr.close();  
@@ -96,150 +89,117 @@ public class CalendarInformation {
         }
     }
     
-    public DefaultListModel<String> getTitleList()
-    {
-        model.removeAllElements();
-        for(int i = 0; i < ev.size(); i++)
-        {
-            model.addElement(ev.get(i).getTitle());
-        }
-        return model;
-    }
-    
-    public DefaultListModel<String> getLocList()
-    {
-        model.removeAllElements();
-        for(int i = 0; i < ev.size(); i++)
-        {
-            model.addElement(ev.get(i).getLoc());
-        }
-        return model;
-    }
-    
-    public DefaultListModel<String> getDateList()
-    {
-        model.removeAllElements();;
-        for(int i = 0; i< ev.size(); i++)
-        {
-            model.addElement(ev.get(i).getDate());
-        }
-        return model;
-    }
-    
     public void addInfo(String title, String loc, String Date)
     {
-        ev.add(new Event(title, loc, Date));
+        this.title.add(title);
+        this.loc.add(loc);
+        this.date.add(Date);
     }
     
-//    public Vector<String> getTitle()
-//    {
-//        return title;
-//    }
-//    
-//    public Vector<String> getLoc()
-//    {
-//        return loc;
-//    }
-//    
-//    public Vector<String> getDate()
-//    {
-//        return date;
-//    }
-    
-    public Vector<Event> getEvent()
+    public Vector<String> getTitle()
     {
-        return ev;
+        return title;
     }
     
-//    public void sortLoc() throws IOException
-//    {
-//        int j = 0;
-//        String loc1 = "";//loc.get(0);
-//        String title1 = "";//title.get(0);
-//        String date2 = "";//date.get(0);
-//        
-//        //String tempLoc = "";
-//       // String tempTitle = "";
-//        //String tempDate = "";
-//        if(!loc.isEmpty())
-//        {
-//            for(int i = 0; i < loc.size(); i++)
-//            {
-//                j = i;
-//                for(int k = i; k < loc.size(); k++)
-//                {
-//                    if(loc.get(j).charAt(0) > loc.get(k).charAt(0))
-//                    {
-//                        j = k;
-//                    }
-//                }
-//                loc1 = loc.get(i);
-//                title1 = title.get(i);
-//                date2 = date.get(i);
-//                
-//                loc.set(i, loc.get(j));
-//                loc.set(j, loc1);
-//                
-//                title.set(i, title.get(j));
-//                title.set(j, title1);
-//                
-//                date.set(i, date.get(j));
-//                date.set(j, date2);
-//            }
-//        }
-//        try{
-//            update();
-//        }
-//        catch(IOException ex)
-//        {
-//            
-//        }
-//        
-//    }
-//    
-//    public void sortTitle() throws IOException
-//    {
-//        int j = 0;
-//        String loc1 = "";//loc.get(0);
-//        String title1 = "";//title.get(0);
-//        String date2 = "";//date.get(0);
-//        
-//        //String tempLoc = "";
-//       // String tempTitle = "";
-//        //String tempDate = "";
-//        if(!title.isEmpty())
-//        {
-//            for(int i = 0; i < title.size(); i++)
-//            {
-//                j = i;
-//                for(int k = i; k < title.size(); k++)
-//                {
-//                    if(title.get(j).charAt(0) > title.get(k).charAt(0))
-//                    {
-//                        j = k;
-//                    }
-//                }
-//                loc1 = loc.get(i);
-//                title1 = title.get(i);
-//                date2 = date.get(i);
-//                
-//                loc.set(i, loc.get(j));
-//                loc.set(j, loc1);
-//                
-//                title.set(i, title.get(j));
-//                title.set(j, title1);
-//                
-//                date.set(i, date.get(j));
-//                date.set(j, date2);
-//            }
-//        }
-//        try{
-//            update();
-//        }
-//        catch(IOException ex)
-//        {
-//            
-//        }
-//        
-//    }
+    public Vector<String> getLoc()
+    {
+        return loc;
+    }
+    
+    public Vector<String> getDate()
+    {
+        return date;
+    }
+    
+    public void sortLoc() throws IOException
+    {
+        int j = 0;
+        String loc1 = "";//loc.get(0);
+        String title1 = "";//title.get(0);
+        String date2 = "";//date.get(0);
+        
+        //String tempLoc = "";
+       // String tempTitle = "";
+        //String tempDate = "";
+        if(!loc.isEmpty())
+        {
+            for(int i = 0; i < loc.size(); i++)
+            {
+                j = i;
+                for(int k = i; k < loc.size(); k++)
+                {
+                    if(loc.get(j).charAt(0) > loc.get(k).charAt(0))
+                    {
+                        j = k;
+                    }
+                }
+                loc1 = loc.get(i);
+                title1 = title.get(i);
+                date2 = date.get(i);
+                
+                loc.set(i, loc.get(j));
+                loc.set(j, loc1);
+                
+                title.set(i, title.get(j));
+                title.set(j, title1);
+                
+                date.set(i, date.get(j));
+                date.set(j, date2);
+            }
+        }
+        try{
+            update();
+        }
+        catch(IOException ex)
+        {
+            
+        }
+        
+    }
+    
+    public void sortTitle() throws IOException
+    {
+        int j = 0;
+        String loc1 = "";//loc.get(0);
+        String title1 = "";//title.get(0);
+        String date2 = "";//date.get(0);
+        
+        //String tempLoc = "";
+       // String tempTitle = "";
+        //String tempDate = "";
+        if(!title.isEmpty())
+        {
+            for(int i = 0; i < title.size(); i++)
+            {
+                j = i;
+                for(int k = i; k < title.size(); k++)
+                {
+                    if(title.get(j).charAt(0) > title.get(k).charAt(0))
+                    {
+                        j = k;
+                    }
+                }
+                loc1 = loc.get(i);
+                title1 = title.get(i);
+                date2 = date.get(i);
+                
+                loc.set(i, loc.get(j));
+                loc.set(j, loc1);
+                
+                title.set(i, title.get(j));
+                title.set(j, title1);
+                
+                date.set(i, date.get(j));
+                date.set(j, date2);
+            }
+        }
+        try{
+            update();
+        }
+        catch(IOException ex)
+        {
+            
+        }
+        
+    }
 }
